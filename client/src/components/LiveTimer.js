b363fc4bb3c6652b6a9c3b420f0cd9a5fe022bf0
Teresa Pham <tkypham@gmail.com>
2021-07-04 18:33:26 +1000

    Pause and progress bar functionality implemented

--- client/src/components/LiveTimer.js
+++ client/src/components/LiveTimer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import Button from './styles/Button';
@@ -8,13 +8,6 @@ const TimerStyles = styled.div`
     .timer {
         font-size: 15.5rem;
         font-family: 'IBM Plex Mono', mono;
-    }
-
-    .current-bar {
-        display: inline-block;
-        background-color: white;
-        height: 3.6rem;
-        width: 80%;
     }
 
     .percentage-bar {
@@ -44,18 +37,41 @@ const TimerStyles = styled.div`
 
 `;
 
+const ProgressBar = styled.div`
+    display: inline-block;
+    background-color: white;
+    height: 3.6rem;
+    width: ${props => props.percent}%;
+`;
+
 function LiveTimer(props) {
 
     const session = props.session.getSession;
     const [timer, setTimer] = useState(0);
-    const [timerStart] = useState(Date.now());
+    const [timerStart, setTimerStart] = useState(Date.now());
     const [isActive, setIsActive] = useState(true);
+    const [goalReached, setGoalReached] = useState(false);
+    const timerRef = useRef(null);
 
     useEffect(() => {
-        setInterval(() => {
+        timerRef.current = setInterval(() => {
             setTimer(Date.now() - timerStart);
-        }, 1000)
+        }, 1000);
     }, [timerStart]);
+
+    const handlePause = () => {
+        if (isActive) {
+            setIsActive(false);
+            clearInterval(timerRef.current);
+        } else {
+            setTimerStart(Date.now() - timer);
+            setIsActive(true);
+        }
+    };
+
+    const handleFinish = () => {
+
+    };
 
     const displaySessionGoal = (timeGoal) => {
         const numHours = Math.floor(timeGoal / 3600);
@@ -70,6 +86,17 @@ function LiveTimer(props) {
         }
     };
 
+    const getPercent = () => {
+        const timeSec = timer/1000;
+        let percent = (timeSec/session.timeGoal).toFixed(2) * 100;
+        
+        if (percent >= 100) {
+            percent = 100;
+            setGoalReached(true);
+        }
+        return percent;
+    };
+
     let seconds = ("0" + (Math.floor(timer / 1000) % 60)).slice(-2);
     let minutes = ("0" + (Math.floor(timer / 60000) % 60)).slice(-2);
     let hours = ("0" + Math.floor(timer / 3600000)).slice(-2);
@@ -81,12 +108,12 @@ function LiveTimer(props) {
             <div className="progress-bar-container">
                 <p className="top-right-label">{displaySessionGoal(session.timeGoal)}</p>
                 <div className="percentage-bar">
-                    <div className="current-bar"></div>
+                    <ProgressBar percent={getPercent}/>
                 </div>
-                <p className="bottom-right-label">Session Goal</p>
+                <p className="bottom-right-label">{ goalReached ? "You reached your goal!" : "Session Goal" }</p>
             </div>
-            <Button className="timer-button">{isActive ? "Finish deep work session" : "Start session"}</Button>
-            <UnderlineLink>{ isActive ? "Pause session" : "Resume session" }</UnderlineLink>
+            <Button className="timer-button" onClick={handleFinish}>Finish deep work session</Button>
+            <UnderlineLink onClick={handlePause}>{ isActive ? "Pause session" : "Resume session" }</UnderlineLink>
         </TimerStyles>
     );
 }
